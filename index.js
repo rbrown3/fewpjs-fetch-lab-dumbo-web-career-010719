4fb71307ec1979faa7bc233d2aa2547f3616d3e9
rbrown3 <rosannabrown@outlook.com>
2019-02-21 10:20:42 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+fetchBooks('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
